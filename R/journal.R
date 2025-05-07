607803b38173296239e86427aef772d52b557ac3
Aldohlys <aldohlys@gmail.com>
2025-05-08 01:00:23 +0200

    Add argts to journal Modify Entry

--- R/journal.R
+++ R/journal.R
@@ -57,21 +57,112 @@ writeJournalEntry <- function(entry) {
 #' It checks first that close is numeric and change is character. Otherwise it just sends the update request to DB.
 #'@returns number of lines modified, if no error it returns 1. It returns 0 if there is an error
 #'@param entryId, integer - entry key to modify into the Journal
+#'@param theme, string
+#'@param date, string
 #'@param sym, string - symbol to be updated
 #'@param close, numeric, equal to last close value for the symbol
 #'@param change, character - percentage change between last day and penultimate day
+#'@param mkt_price, double
+#'@param mkt_change, character - percentage change between last day and penultimate day
 #'@param text, string - comment, remark
 #'@export
-modifyJournalEntry <- function(entryId, sym, close, change, text) {
-  if( (!(is.numeric(close))) | (!(is.character(change))) ) Tbasics::display_error_message("Please provide numerical value for close and percentage for change")
-  else {
+modifyJournalEntry <- function(entryId, theme, date, sym, close, change, mkt_price, mkt_change, text) {
+
+  ### Initialize update string and params list
+  sql <- ""
+  params <- list()
+
+  if (is.character(sym) && length(sym) > 1) {
+    sql <- "UPDATE Journal SET sym = ?"
+    params = list(sym)
+  }
+
+  if (is.numeric(close)) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET close = ?"
+      params <- list(close)
+    }
+    else {
+      sql <- paste(sql, ", close = ?")
+      params <- append(params, close)
+    }
+  }
+
+  if (is.character(change) && length(change) > 1) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET change = ?"
+      params <- list(change)
+    }
+    else {
+      sql <- paste(sql, ", change = ?")
+      params <- append(params, change)
+    }
+  }
+
+  if (is.numeric(mkt_price)) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET mkt_price = ?"
+      params <- list(mkt_price)
+    }
+    else {
+      sql <- paste(sql, ", mkt_price = ?")
+      params <- append(params, mkt_price)
+    }
+  }
+
+  if (is.character(mkt_change) && length(mkt_change) > 1) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET mkt_change = ?"
+      params <- list(mkt_change)
+    }
+    else {
+      sql <- paste(sql, ", mkt_change = ?")
+      params <- append(params, mkt_change)
+    }
+  }
+
+  if (is.character(theme) && length(theme) > 1) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET theme = ?"
+      params <- list(theme)
+    }
+    else {
+      sql <- paste(sql, ", theme = ?")
+      params <- append(params, theme)
+    }
+  }
+
+  if (is.character(date) && length(date) > 1) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET date = ?"
+      params <- list(date)
+    }
+    else {
+      sql <- paste(sql, ", date = ?")
+      params <- append(params, date)
+    }
+  }
+
+  if (is.character(text) && length(text) > 1) {
+    if (length(sql) == 0) {
+      sql <- "UPDATE Journal SET text = ?"
+      params <- list(text)
+    }
+    else {
+      sql <- paste(sql, ", text = ?")
+      params <- append(params, text)
+    }
+  }
+
+  if (length(sql) != 0) {
+    sql <- paste(sql, "WHERE entryID =?;")
     conn <- DBI::dbConnect(RSQLite::SQLite(), config::get("DB"))
-    status <- DBI::dbExecute(conn, "UPDATE Journal SET sym = ?, close = ?, change =?, text = ? WHERE entryId = ?;",
-                             params=list(sym, close, change, text, entryId))
+    status <- DBI::dbExecute(conn, sql, params)
     DBI::dbDisconnect(conn)
     status
   }
 
+  else return(0)
 }
 
 #'   deleteJournalEntry
