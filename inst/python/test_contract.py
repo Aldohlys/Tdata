71033878e34a791254b92ba76864d9482093a47c
Aldohlys <aldohlys@gmail.com>
2025-09-14 23:28:21 +0200

    Increment version number to 5.7.1

--- inst/python/test_contract.py
+++ inst/python/test_contract.py
@@ -13,7 +13,7 @@ from tdata_py.chains_manager import getChains, getAllStrikes
 from tdata_py.dividend_utils import getNTMDividend
 from tdata_py.contract import getValue, getOptValue, getStraddleValue, getStrikesfromExpDate
 
-### 1. Test getValue with one symbol ===
+print("1. Test getValue with one symbol ===")
 getValue("SAF", close = False)
 getValue("SAF", close = True)
 getValue("ESTX50", close = True)
@@ -24,24 +24,24 @@ getValue("SR3Z6", secType="FUT", expiration = "202612")
 getValue("MSFU5", secType="FUT", expiration="202509")
 getValue("MSFU5")
 
-### 2. Test getValue with multiple symbols
+print("2. Test getValue with multiple symbols ==")
 getValue(["SAF", "SPY", "ESTX50", "SPX"], close = False)
 
-#### 3. Test getOptValue with one strike
+print("3. Test getOptValue with one strike ===")
 getOptValue("SAF", expiration="20251017", strikes=270.0, right='P', currency="EUR")
 getOptValue("SAF", expiration="20251010", strikes=275.0, right='P', currency="EUR")
 getOptValue("SAF", expiration="20250919", strikes=280.0, right='P', currency="EUR")
 
-#### 4. Test getOptValue with multiple strikes
+print("4. Test getOptValue with multiple strikes===)
 getOptValue("SAF", expiration="20251017", strikes=[280.0, 270.0], right='P', currency="EUR")
 getOptValue("SAF", expiration="20251010", strikes=[280.0, 275.0, 270.0], right='P', currency="EUR")
 getOptValue("ESTX50", expiration="20251010", strikes=5300, right='P', currency='EUR')
 getOptValue("ESTX50", expiration="20251017", strikes=[5300, 5325, 5275], right='P', currency='EUR')
 
-#### 5. Test getStraddleValue with multiple strikes
+print("5. Test getStraddleValue with multiple strikes ===")
 getStraddleValue("URA",  expiration="20250919", strike=39)
 getStraddleValue("VXX",  expiration="20250919", strike=36)
 
-#### 6. Test getStrikesfromExpDate
+print("6. Test getStrikesfromExpDate ===")
 getStrikesfromExpDate("SPX")
 getStrikesfromExpDate("TTE", expdate="20250919")
